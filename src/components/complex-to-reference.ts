4be83ad28378d199b2e80aa7b92bd7c2f5ea8bb2
tq_xyy <m17097231932@163.com>
2026-08-10 22:40:51 +0800

    feat: transform complex call expression arguments to reference

--- src/components/complex-to-reference.ts
+++ src/components/complex-to-reference.ts
@@ -1,6 +1,31 @@
 import * as t from '@babel/types'
 import { defineComponent } from '../base'
 
+function checkObjectLevelMoreThan(obj: t.Node, level: number) {
+    const queue: { node: t.Node; depth: number }[] = [{ node: obj, depth: 0 }]
+
+    while (queue.length > 0) {
+        const { node, depth } = queue.shift()!
+
+        if (depth > level) return true
+
+        if (node == null || typeof node !== 'object') continue
+
+        const children = Array.isArray(node) ? node : Object.values(node)
+
+        for (const child of children) {
+            if (child != null && typeof child === 'object') {
+                if (depth === level) {
+                    return true
+                }
+                queue.push({ node: child, depth: depth + 1 })
+            }
+        }
+    }
+
+    return false
+}
+
 export default defineComponent({
     MemberExpression(path) {
         const n = path.node
@@ -12,5 +37,25 @@ export default defineComponent({
             path.scope.push({ id, init: n.object })
             n.object = id
         }
+    },
+    CallExpression(path) {
+        if (!path.parentPath.isExpressionStatement()) {
+            return
+        }
+        path.node.arguments.forEach((arg, index) => {
+            if (
+                t.isExpression(arg) &&
+                !t.isFunction(arg) &&
+                checkObjectLevelMoreThan(arg, 9)
+            ) {
+                let id = path.scope.generateUidIdentifier('callArgs')
+                path.node.arguments[index] = id
+                path.parentPath.insertBefore(
+                    t.variableDeclaration('const', [
+                        t.variableDeclarator(id, arg),
+                    ])
+                )
+            }
+        })
     },
 })
